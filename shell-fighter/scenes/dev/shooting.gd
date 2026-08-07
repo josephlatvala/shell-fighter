e98067163d06e9b7809aaa7bc877e36c1ca02724
Joseph Latvala <joseph.latvala@posteo.com>
2026-08-06 23:59:41 -0400

    add comments

--- shell-fighter/scenes/dev/shooting.gd
+++ shell-fighter/scenes/dev/shooting.gd
@@ -11,5 +11,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	# Get the mouse position relative to the center of the camera
 	var mousePosition = get_viewport().get_mouse_position() as Vector2i - get_window().size / 2
+	# Set the muzzle position to be a constant displacement away in the direction of the mouse
 	muzzle.position = mousePosition / sqrt(mousePosition.x**2 + mousePosition.y**2) * muzzleDistance
