7347750751a047ffe3520c6fe84c7adce5c6d23c
singhp24-png <singhp24@udayton.edu>
2026-08-07 22:21:59 -0400

    fix: add i-frames

--- shell-fighter/scenes/entities/player/player.gd
+++ shell-fighter/scenes/entities/player/player.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var hurtbox: Area2D = $Hurtbox
+
 @export var speed: int = 5
 
-var screenSize : Vector2
+var screenSize: Vector2
 
 func _ready() -> void:
 	screenSize = get_viewport_rect().size
@@ -14,8 +15,14 @@ func _ready() -> void:
 
 # Deals with player damage
 func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if health_component.is_invincible:
+		return
 	if body.is_in_group("enemy") and "contact_damage" in body:
+		print("Ouchie")
 		health_component.take_damage(body.contact_damage)
+		hurtbox.set_deferred("monitoring", false)
+		await get_tree().create_timer(health_component.invincibility_time).timeout
+		hurtbox.monitoring = true
 
 func _on_died() -> void:
 	# TODO: hook up to a game over
@@ -24,7 +31,7 @@ func _on_died() -> void:
 func get_input():
 	var directionalInput = Input.get_vector("leftMovement", "rightMovement", "upMovement", "downMovement")
 	velocity = directionalInput.normalized() * speed
-	
+
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
