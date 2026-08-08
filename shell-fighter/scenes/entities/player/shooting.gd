bb84c10f294d8416e57d3e80ff048bbce0717ebc
Joseph Latvala <joseph.latvala@posteo.com>
2026-08-08 14:40:22 -0400

    make the gun use it too

--- shell-fighter/scenes/entities/player/shooting.gd
+++ shell-fighter/scenes/entities/player/shooting.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 # TODO: Consider making these depend on a selectable gun
-const muzzleDistance: float = 30
+const muzzleDistance: float = 15
 const bulletVelocityMagnitude: float = 100
 const bulletTimeToLive: float = 0.3
 const bulletDamage: int = 10
@@ -15,7 +15,7 @@ var shootDirection: Vector2 = Vector2(0,1) # Should be a unit vector
 
 @onready var muzzle: Marker2D = $Muzzle
 @onready var player: Node2D = $".."
-@onready var sprite: Sprite2D = $Muzzle/Sprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $Muzzle/AnimatedSprite2D
 @onready var ammo_component: AmmoComponent = $"../AmmoComponent"
 
 @onready var debug_mouse_tracker: Line2D = $DebugMouseTracker
@@ -35,17 +35,16 @@ func _process(_delta: float) -> void:
 	
 	debug_aim_tracker.set_point_position(1, muzzle.position * 16)
 	
-	sprite.frame = gunTextureIndex(muzzle.rotation)
+	AssignGunTexture(muzzle.rotation)
 
-func gunTextureIndex(angle: float):
+func AssignGunTexture(angle: float):
 	# Divide it into buckets of size PI/4,
 	# shift it up by PI/8 so the cardinal directoins are in the center of each bucket,
 	# and tuncate to get the index
 	var bucket = ((angle + PI / 8) / (PI / 4)) as int % 8
 	
-	print(bucket)
-	const texturePermutation: Array = [16, 16, 17, 18, 18, 18, 19, 16]
-	return texturePermutation[bucket]
+	const texturePermutation: Array = ["gun_right", "gun_right", "gun_down", "gun_left", "gun_left", "gun_left", "gun_up", "gun_right"]
+	animated_sprite_2d.play(texturePermutation[bucket])
 
 func _input(event):
 	if event is InputEventMouseButton:
