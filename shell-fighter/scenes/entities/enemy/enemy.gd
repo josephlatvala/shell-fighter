5c15c6661b9e2962d8eb32f1cbf1fccd75f31dc0
singhp24-png <singhp24@udayton.edu>
2026-08-07 20:18:08 -0400

    feat: add player damage

--- shell-fighter/scenes/entities/enemy/enemy.gd
+++ shell-fighter/scenes/entities/enemy/enemy.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var contact_damage: int = 10
+
 @onready var chase_component: ChaseComponent = $ChaseComponent
 @onready var health_component: HealthComponent = $HealthComponent
 
@@ -8,7 +10,7 @@ func _ready() -> void:
 	if player:
 		chase_component.start_chasing(player)
 	health_component.died.connect(_on_died)
-	
+
 func _physics_process(_delta: float) -> void:
 	velocity = chase_component.get_velocity(global_position)
 	move_and_slide()
