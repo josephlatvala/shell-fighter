3ea163017507a91267b0ce9dab5f34f3624d64da
CardBladerGamer <jaredrmiller2005@gmail.com>
2026-08-07 21:30:00 -0400

    Player movement added.

--- shell-fighter/scenes/entities/player/player.gd
+++ shell-fighter/scenes/entities/player/player.gd
@@ -2,8 +2,13 @@ extends CharacterBody2D
 
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var hurtbox: Area2D = $Hurtbox
+@export var speed: int = 5
+
+var screenSize : Vector2
 
 func _ready() -> void:
+	screenSize = get_viewport_rect().size
+	position = screenSize / 2
 	health_component.died.connect(_on_died)
 	hurtbox.body_entered.connect(_on_hurtbox_body_entered)
 
@@ -15,3 +20,11 @@ func _on_hurtbox_body_entered(body: Node2D) -> void:
 func _on_died() -> void:
 	# TODO: hook up to a game over
 	queue_free()
+
+func get_input():
+	var directionalInput = Input.get_vector("leftMovement", "rightMovement", "upMovement", "downMovement")
+	velocity = directionalInput.normalized() * speed
+	
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
