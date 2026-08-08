e0121c812d18502a6dbc8b0edc43903326b0e563
singhp24-png <singhp24@udayton.edu>
2026-08-07 20:00:15 -0400

    feat: add damage to enemy

--- shell-fighter/scenes/entities/bullet/bullet.gd
+++ shell-fighter/scenes/entities/bullet/bullet.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 # TODO: Consider making bullets expire when they go off the screen rather than if a duration expires
 const timeToLive: float = 3
+const damage: int = 10
 
 @onready var rigid_body_2d: RigidBody2D = $RigidBody2D
+@onready var area_2d: Area2D = $RigidBody2D/Area2D
 @onready var timeLived = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	area_2d.body_entered.connect(_on_body_entered) # Connect area 2d object with function
 	pass # Replace with function body.
 
 
@@ -15,4 +18,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	timeLived += delta
 	if timeLived >= timeToLive:
+		queue_free()
+
+# Called when an Area node intersects with the bullet.
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		var health_component := body.get_node_or_null("HealthComponent") as HealthComponent
+		if health_component:
+			health_component.take_damage(damage)
 		queue_free()
--- shell-fighter/scenes/entities/enemy/enemy.gd
+++ shell-fighter/scenes/entities/enemy/enemy.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
 @onready var chase_component: ChaseComponent = $ChaseComponent
+@onready var health_component: HealthComponent = $HealthComponent
 
 func _ready() -> void:
 	var player := get_tree().get_first_node_in_group("player")
 	if player:
 		chase_component.start_chasing(player)
-
+	health_component.died.connect(_on_died)
+	
 func _physics_process(_delta: float) -> void:
 	velocity = chase_component.get_velocity(global_position)
 	move_and_slide()
+
+func _on_died() -> void:
+	queue_free()
