178e33f2d05e2904b34e0e434c4ee8d5ce742221
Joseph Latvala <joseph.latvala@posteo.com>
2026-08-09 00:37:16 -0400

    add bullets

--- shell-fighter/scenes/pickup/ammo_pickup.gd
+++ shell-fighter/scenes/pickup/ammo_pickup.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 # Which pool this pickup refills.
 @export var bullet_data: BulletData
 @export var ammo_amount: int = 5
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	if bullet_data.texture:
+		sprite_2d.texture = bullet_data.texture
 
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("player") or not body.has_node("AmmoComponent"):
